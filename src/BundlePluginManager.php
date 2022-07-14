5c37585098909f40f1b9663b36e54461af7d4745
Michael Strelan <mstrelan@gmail.com>
2022-07-14 11:39:39 +1000

    Constants

--- src/BundlePluginManager.php
+++ src/BundlePluginManager.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\bca;
 
+use Drupal\bca\Annotation\Bundle;
 use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Plugin\DefaultPluginManager;
 
@@ -13,18 +15,15 @@ use Drupal\Core\Plugin\DefaultPluginManager;
  */
 class BundlePluginManager extends DefaultPluginManager {
 
+  protected const SUBDIR = 'Entity';
+  protected const CACHE_KEY = 'bca_bundle_classes';
+
   /**
    * {@inheritdoc}
    */
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
-    parent::__construct(
-      'Entity',
-      $namespaces,
-      $module_handler,
-      'Drupal\Core\Entity\EntityInterface',
-      'Drupal\bca\Annotation\Bundle'
-    );
-    $this->setCacheBackend($cache_backend, 'bca_bundle_classes');
+    parent::__construct(static::SUBDIR, $namespaces, $module_handler, EntityInterface::class, Bundle::class);
+    $this->setCacheBackend($cache_backend, static::CACHE_KEY);
   }
 
 }
